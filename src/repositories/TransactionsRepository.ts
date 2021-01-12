bb6d47d34ef80e74b5ec403c4f4c7fd1fb61f680
Paulo Ferrari <pf.devcon@gmail.com>
2021-01-12 14:00:16 -0300

    create transactions routes, repository and service to abstract create transaction details

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import Transaction from '../models/Transaction';
 
 interface Balance {
@@ -14,15 +15,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const initialValue: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    const balance = this.transactions.reduce<Balance>(
+      (balanceAcc, transaction): Balance => {
+        if (transaction.type === 'income') {
+          balanceAcc.income += transaction.value;
+        }
+
+        if (transaction.type === 'outcome') {
+          balanceAcc.outcome += transaction.value;
+        }
+
+        return balanceAcc;
+      },
+      initialValue,
+    );
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
